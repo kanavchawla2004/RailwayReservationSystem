d6bc7347f1d7accbb80f9f0f2c1da12e1a289a96
Kanav Chawla <kanav.chawla@iongroup.com>
2025-02-27 21:59:46 +0530

    added basic structure

---
+++ src/main/java/com/ion/App.java
@@ -0,0 +1,10 @@
+package com.ion;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/ion/BookingTicket.java
@@ -0,0 +1,5 @@
+package com.ion;
+
+public class BookingTicket {
+    
+}
