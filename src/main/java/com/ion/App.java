cfa72d164046364d4a5ae26697397c8a525529d6
Kanav Chawla <kanav.chawla@iongroup.com>
2025-02-28 02:06:53 +0530

    Added basic recursive lock while booking the BookingTicket

--- src/main/java/com/ion/App.java
+++ src/main/java/com/ion/App.java
@@ -4,20 +4,26 @@ package com.ion;
 public class App {
     public static void main( String[] args ){
         // System.out.print("0");
+        // Passenger kPassenger = new Passenger();
         Passenger kanavPassenger = new Passenger();
         kanavPassenger.setAge(21);
         kanavPassenger.setName("Kanav");
         kanavPassenger.setVip(false);
         kanavPassenger.setTickets(5);
 
+        Passenger notPassenger = new Passenger();
+        notPassenger.setAge(22);
+        notPassenger.setName("notKanav");
+        notPassenger.setVip(true);
+        notPassenger.setTickets(10);
+
         BookingThread kanavThread = new BookingThread(kanavPassenger);
+        BookingThread kanaThread2 = new BookingThread(notPassenger);
         kanavThread.start();
-        // kanavThread.start();
-        // Passenger kanav2Passenger = new Passenger();
-        // kanavPassenger.setAge(21);
-        // kanavPassenger.setName("Kanav2");
-        // kanavPassenger.setVip(true);
-        // kanavPassenger.bookTicket("kanav",5);
+        kanaThread2.start();
+
+        // kanavThread.join();
+        // kanaThread2.join();
 
         // BookingTicket.bookTicket(kanavPassenger.getName(), kanavPassenger.getTickets());
     }
--- src/main/java/com/ion/BookingTicket.java
+++ src/main/java/com/ion/BookingTicket.java
@@ -1,17 +1,26 @@
 package com.ion;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BookingTicket {
     public static int totalSeats = 50;
     public static int bookedSeats = 0;
     public static int totalAvailableSeats = totalSeats - bookedSeats;
+    public static Lock lock = new ReentrantLock();
     public static boolean bookTicket(String name,int num){
+        lock.lock();
+        try{
         if((num <= totalAvailableSeats) && num>0){
             bookedSeats+=num;
             totalAvailableSeats-=num;
             System.out.println(totalAvailableSeats);
+            return true;
         }else{
             return false;
+        }}finally{
+            lock.unlock();
         }
-        return true;
+        
     }
 }
