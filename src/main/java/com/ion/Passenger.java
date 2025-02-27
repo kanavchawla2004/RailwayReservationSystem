955456088d40a56017707b367c5718e0d1ae09a0
Kanav Chawla <kanav.chawla@iongroup.com>
2025-02-27 22:33:40 +0530

    Passenger: Added details about passenger

--- src/main/java/com/ion/Passenger.java
+++ src/main/java/com/ion/Passenger.java
@@ -1,5 +1,27 @@
 package com.ion;
 
 public class Passenger {
+    public String name;
+    public int age;
+    public boolean isVip;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public boolean isVip() {
+        return isVip;
+    }
+    public void setVip(boolean isVip) {
+        this.isVip = isVip;
+    }
     
 }
