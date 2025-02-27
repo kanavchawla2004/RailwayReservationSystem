c10f2254d9a7566ca36723c4853c1ffe6f77c523
Kanav Chawla <kanav.chawla@iongroup.com>
2025-02-27 22:57:40 +0530

    Added basic coding logic to book seats

--- src/main/java/com/ion/App.java
+++ src/main/java/com/ion/App.java
@@ -5,6 +5,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Passenger kanavPassenger = new Passenger();
+        kanavPassenger.setAge(21);
+        kanavPassenger.setName("Kanav");
+        kanavPassenger.setVip(false);
+        // kanavPassenger.bookTicket("kanav",5);
+        BookingTicket.bookTicket(kanavPassenger.getName(), 5);
     }
 }
--- src/main/java/com/ion/BookingTicket.java
+++ src/main/java/com/ion/BookingTicket.java
@@ -1,5 +1,17 @@
 package com.ion;
 
 public class BookingTicket {
-    
+    public static int totalSeats = 50;
+    public static int bookedSeats = 0;
+    public static int totalAvailableSeats = totalSeats - bookedSeats;
+    public static boolean bookTicket(String name,int num){
+        if((num <= totalAvailableSeats) && num>0){
+            bookedSeats+=num;
+            totalAvailableSeats-=num;
+            System.out.println(totalAvailableSeats);
+        }else{
+            return false;
+        }
+        return true;
+    }
 }
